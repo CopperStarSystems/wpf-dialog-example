22e2d8c7c460c8354e29283018f6e80d88b6d5ec
Ed Mays <copperstarsystems@gmail.com>
2024-08-04 18:11:17 +0000

    Stub out save and cancel commands

--- ModalDialogExample/ViewModels/MoldDetailDialogViewModel.cs
+++ ModalDialogExample/ViewModels/MoldDetailDialogViewModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ModalDialogExample.ViewModels
 {
@@ -31,5 +32,9 @@ namespace ModalDialogExample.ViewModels
                 return string.Format("Mold Details: {0}", SelectedMold.Name);
             }
         }
+
+        public ICommand SaveCommand { get; }
+
+        public ICommand CancelCommand { get; }
     }
 }
